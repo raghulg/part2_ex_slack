d4f00aa2191f075cb99f67b5eab1945ed5e95c01
root <raghul.gunasekaran@gmail.com>
2018-02-05 14:19:32 +0000

    changed django debug mode to false

--- eventStore/eventStore/settings.py
+++ eventStore/eventStore/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'v#efk*cbf1va-1e9ye68iynv#is(5=j(p2pni)7fzvhyt1a1hh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['54.172.96.124']
 
